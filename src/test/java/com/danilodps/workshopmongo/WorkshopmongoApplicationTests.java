15c48cd74b4783158159028af10ad43ada881bdb
Danilo-dps <danilodpsnilo@outlook.com>
2024-09-21 23:53:34 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/danilodps/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilodps.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
